ccc14147ffbb695f31a7ac4e0331a916de88b804
lwmacct <lwmacct@icloud.com>
2025-08-13 19:59:01 +0800

    -

--- dev-2508130/apps/file/apps/data/init.d/00-init.sh
+++ dev-2508130/apps/file/apps/data/init.d/00-init.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 {
-  find /host/run -name docker.sock -exec ln -sf /host/run/docker.sock /var/run/docker.sock \;
+  find /host/run -maxdepth 1 -name docker.sock -exec ln -sf {} /var/run/docker.sock \;
   ln -sf /apps/data/workspace/w.code-workspace /root/w.code-workspace
 
 }
