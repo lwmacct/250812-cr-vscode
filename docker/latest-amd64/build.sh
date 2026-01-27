b5b52cbb03171543344272957c1c12050c75da3f
lwmacct <lwmacct@icloud.com>
2026-01-27 19:30:03 +0800

    chore 2026-01-27 19:30:03 CST

--- docker/latest-amd64/build.sh
+++ docker/latest-amd64/build.sh
@@ -3,15 +3,15 @@
 # document https://www.yuque.com/lwmacct/docker/buildx
 
 __main() {
-    {
-        _sh_path=$(realpath "$(ps -p $$ -o args= 2>/dev/null | awk '{print $2}')")    # 当前脚本路径
-        _dir_name=$(echo "$_sh_path" | awk -F '/' '{print $(NF-1)}')                  # 当前目录名
-        _pro_name=$(git remote get-url origin | head -n1 | xargs -r basename -s .git) # 当前仓库名
-        _image="${_pro_name}:$_dir_name"
-    }
+  {
+    _sh_path=$(realpath "$(ps -p $$ -o args= 2>/dev/null | awk '{print $2}')")    # 当前脚本路径
+    _dir_name=$(echo "$_sh_path" | awk -F '/' '{print $(NF-1)}')                  # 当前目录名
+    _pro_name=$(git remote get-url origin | head -n1 | xargs -r basename -s .git) # 当前仓库名
+    _image="${_pro_name}:$_dir_name"
+  }
 
-    _dockerfile=$(
-        cat <<"EOF"
+  _dockerfile=$(
+    cat <<"EOF"
 # https://hub.docker.com/_/ubuntu
 FROM ubuntu:noble-20251013
 LABEL maintainer="https://github.com/lwmacct"
@@ -236,6 +236,14 @@ RUN echo "软链接 cron.d" ; \
     chmod 700 /root/.ssh; \
     echo "StrictHostKeyChecking no" >> /root/.ssh/config;
 
+RUN set -eux; \
+    echo "20260127"; \
+    echo "https://github.com/anthropics/claude-code/blob/main/CHANGELOG.md"; \
+    npm config set cache /tmp/npm-cache; \
+    npm install -g --no-cache @openai/codex @google/gemini-cli; \
+    npm cache clean --force; \
+    rm -rf ~/.npm /tmp/npm-cache
+
 RUN echo "软链接 cron.d" ; \
     rm -rf /etc/cron.d/; \
     ln -sf /app/data/cron.d/ /etc/cron.d; \
@@ -264,49 +272,49 @@ LABEL org.opencontainers.image.source=$_ghcr_source
 LABEL org.opencontainers.image.description="专为 VSCode 容器开发环境构建"
 LABEL org.opencontainers.image.licenses=MIT
 EOF
-    )
-    {
-        cd "$(dirname "$_sh_path")" || exit 1
-        echo "$_dockerfile" >Dockerfile
+  )
+  {
+    cd "$(dirname "$_sh_path")" || exit 1
+    echo "$_dockerfile" >Dockerfile
 
-        _ghcr_source=$(git remote get-url origin | head -n1 | sed 's|git@github.com:|https://github.com/|' | sed 's|.git$||')
-        sed -i "s|\$_ghcr_source|$_ghcr_source|g" Dockerfile
+    _ghcr_source=$(git remote get-url origin | head -n1 | sed 's|git@github.com:|https://github.com/|' | sed 's|.git$||')
+    sed -i "s|\$_ghcr_source|$_ghcr_source|g" Dockerfile
+  }
+  {
+    if command -v sponge >/dev/null 2>&1; then
+      jq 'del(.credsStore)' ~/.docker/config.json | sponge ~/.docker/config.json
+    else
+      jq 'del(.credsStore)' ~/.docker/config.json >~/.docker/config.json.tmp && mv ~/.docker/config.json.tmp ~/.docker/config.json
+    fi
+  }
+  {
+    _registry="ghcr.io/lwmacct" # 托管平台, 如果是 docker.io 则可以只填写用户名
+    _repository="$_registry/$_image"
+    _buildcache="$_registry/$_pro_name:cache"
+    echo "image: $_repository"
+    echo "cache: $_buildcache"
+    echo "-----------------------------------"
+    docker buildx build --builder default --platform linux/amd64 -t "$_repository" --network host --progress plain --load . && {
+      # true/false
+      if false; then
+        docker rm -f sss
+        docker run -itd --name=sss \
+          --restart=none \
+          --network=host \
+          --privileged=false \
+          "$_repository"
+        docker exec -it sss bash
+      fi
     }
-    {
-        if command -v sponge >/dev/null 2>&1; then
-            jq 'del(.credsStore)' ~/.docker/config.json | sponge ~/.docker/config.json
-        else
-            jq 'del(.credsStore)' ~/.docker/config.json >~/.docker/config.json.tmp && mv ~/.docker/config.json.tmp ~/.docker/config.json
-        fi
-    }
-    {
-        _registry="ghcr.io/lwmacct" # 托管平台, 如果是 docker.io 则可以只填写用户名
-        _repository="$_registry/$_image"
-        _buildcache="$_registry/$_pro_name:cache"
-        echo "image: $_repository"
-        echo "cache: $_buildcache"
-        echo "-----------------------------------"
-        docker buildx build --builder default --platform linux/amd64 -t "$_repository" --network host --progress plain --load . && {
-            # true/false
-            if false; then
-                docker rm -f sss
-                docker run -itd --name=sss \
-                    --restart=none \
-                    --network=host \
-                    --privileged=false \
-                    "$_repository"
-                docker exec -it sss bash
-            fi
-        }
-        docker push "$_repository"
+    docker push "$_repository"
 
-    }
+  }
 }
 
 __main
 
 __help() {
-    cat >/dev/null <<"EOF"
+  cat >/dev/null <<"EOF"
 这里可以写一些备注
 
 ghcr.io/lwmacct/250812-cr-vscode:latest-amd64
