8d58832a97e87022a3a9e4805a1987820689fe5e
lwmacctw <lwmacct@icloud.com>
2026-07-05 01:39:30 +0800

    chore 2026-07-05 01:39:30 CST

--- asset/files/root/.zshrc.d/0003-nodejs.sh
+++ asset/files/root/.zshrc.d/0003-nodejs.sh
@@ -2,25 +2,20 @@
 # author https://github.com/lwmacct
 # 判断是否存在 /app/data/root/.nvm
 
-{
-  # fnm
-  FNM_PATH="/root/.local/share/fnm"
-  if [ -d "$FNM_PATH" ]; then
-    export PATH="$FNM_PATH:$PATH"
-    eval "$(fnm env --use-on-cd --shell zsh)"
-    exit 0
-  fi
-}
+# fnm
+FNM_PATH="/root/.local/share/fnm"
+if [ -d "$FNM_PATH" ]; then
+  export PATH="$FNM_PATH:$PATH"
+  eval "$(fnm env --use-on-cd --shell zsh)"
+  exit 0
+fi
 
-{
-  # nvm
-  if [ -d "/app/data/root/.nvm" ]; then
-    export NVM_DIR="/app/data/root/.nvm"
-  fi
-  if [ -d "$HOME/.nvm" ]; then
-    export NVM_DIR="$HOME/.nvm"
-  fi
+# nvm
+if [ -d "/app/data/root/.nvm" ]; then
+  export NVM_DIR="/app/data/root/.nvm"
+fi
+if [ -d "$HOME/.nvm" ]; then
+  export NVM_DIR="$HOME/.nvm"
+fi
 
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-
-}
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
